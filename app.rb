25b738a7af4020b29f681712519f988875087816
c0nniewang <connie.wang303@gmail.com>
2017-11-22 13:15:42 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index    
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @pup_name = params["name"]
+    @pup_breed = params["breed"]
+    @pup_age = params["age"]
+
+    erb :display_puppy
+  end
+
 end
